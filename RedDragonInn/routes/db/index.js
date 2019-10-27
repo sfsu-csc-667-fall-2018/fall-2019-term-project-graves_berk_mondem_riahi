dd4192ee134547c7ab390eb3dabc67ae1bb74712
Graves <theredhead242@gmail.com>
2019-10-26 17:16:32 -0700

    pushing to heroku

--- RedDragonInn/routes/db/index.js
+++ RedDragonInn/routes/db/index.js
@@ -1,6 +1,7 @@
 const pgp = require("pg-promise")();
 
 //TODO: get this from the DATABASE_URL variable in .env, can't seem to figure out how to get it there now
+
 const connection = pgp("postgres://Graves:12@localhost:5432/RedDragonInn");
 
 module.exports = connection;
