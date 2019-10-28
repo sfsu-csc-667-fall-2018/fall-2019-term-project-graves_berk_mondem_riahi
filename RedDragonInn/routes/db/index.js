a1c2a8a9a105eb431dcf341c67c26ed8fcf84b27
Graves <theredhead242@gmail.com>
2019-10-27 21:36:28 -0700

    stable milestone 2

--- RedDragonInn/routes/db/index.js
+++ RedDragonInn/routes/db/index.js
@@ -1,7 +1,5 @@
 const pgp = require("pg-promise")();
 
-//TODO: get this from the DATABASE_URL variable in .env, can't seem to figure out how to get it there now
-
 const connection = pgp(process.env.DATABASE_URL);
 
 module.exports = connection;
