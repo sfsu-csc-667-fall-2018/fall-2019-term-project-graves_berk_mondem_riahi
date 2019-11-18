60a8773d29cbe0f61a94685bbaec396fbb4281fb
Yaboigraves <theredhead242@gmail.com>
2019-11-17 23:56:57 -0800

    socket.io prototype functional on lobby route

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-const io = require("socket.io");
-const app = express();
+
+// c\onst socketIO = require("socket.io");
+
+// const io = socketIO(app);
 
 if (process.env.NODE_ENV === "development") {
   require("dotenv").config();
@@ -18,7 +20,7 @@ const testsRouter = require("./routes/tests/index");
 const registrerRouter = require("./routes/register/index");
 const lobbyRouter = require("./routes/lobby/index");
 //Had it here, and it was set to undefined
-
+const app = express();
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
---
+++ socket/index.js
@@ -0,0 +1,17 @@
+const socketIo = require("socket.io");
+
+const init = (app, server) => {
+  const io = socketIo(server);
+
+  app.set("io", io);
+
+  io.on("connection", socket => {
+    console.log("client connected");
+
+    socket.on("disconnect", data => {
+      console.log("client disconnected");
+    });
+  });
+};
+
+module.exports = { init };
