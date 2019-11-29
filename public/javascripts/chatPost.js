307e6f0fb296ee96a61c1bd5a7b3c45939682441
Yaboigraves <theredhead242@gmail.com>
2019-11-28 19:23:56 -0800

    room creation prototype, simple room list

--- public/javascripts/chatPost.js
+++ public/javascripts/chatPost.js
@@ -1,12 +1,25 @@
 $(function () {
     var socket = io();
-    $('form').submit(function () {
-        socket.emit('chat message', $('#m').val());
-        $('#m').val('');
+    $('form[name="messageForm"]').submit(function () {
+        socket.emit('chat message', $('#message').val());
+        $('#message').val('');
         return false;
     });
     socket.on('chat message', function (msg) {
         $('#messages').append($('<li>').text(msg));
         window.scrollTo(0, document.body.scrollHeight);
     });
+
+    $('form[name="createRoomForm"]').submit(function () {
+        socket.emit('create room', $('#room').val(), $('#password').val());
+        $('#room').val('');
+        $('#password').val('');
+        return false;
+    });
+    socket.on('create room', function (room, password) {
+        $('#roomList').append($('<li>').text("Room Name: " + room + " pass: " + password));
+        //window.scrollTo(0, document.body.scrollHeight);
+    });
+
+
 });
