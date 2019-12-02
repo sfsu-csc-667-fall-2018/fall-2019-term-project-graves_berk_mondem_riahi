cfd46c3b03a49fe767937cd3d7798ee3a3577c6d
Graves <theredhead242@gmail.com>
2019-12-01 23:34:48 -0800

    prototype html for joining rooms

--- public/javascripts/chatPost.js
+++ public/javascripts/chatPost.js
@@ -1,25 +1,39 @@
-$(function () {
-    var socket = io();
-    $('form[name="messageForm"]').submit(function () {
-        socket.emit('chat message', $('#message').val());
-        $('#message').val('');
-        return false;
-    });
-    socket.on('chat message', function (msg) {
-        $('#messages').append($('<li>').text(msg));
-        window.scrollTo(0, document.body.scrollHeight);
-    });
+//todo : remove all these repsonsibilities and defer them somewhere else
 
-    $('form[name="createRoomForm"]').submit(function () {
-        socket.emit('create room', $('#room').val(), $('#password').val());
-        $('#room').val('');
-        $('#password').val('');
-        return false;
-    });
-    socket.on('create room', function (room, password) {
-        $('#roomList').append($('<li>').text("Room Name: " + room + " pass: " + password));
-        //window.scrollTo(0, document.body.scrollHeight);
-    });
+$(function() {
+  var socket = io();
+  $('form[name="messageForm"]').submit(function() {
+    socket.emit("chat message", $("#message").val());
+    $("#message").val("");
+    return false;
+  });
+  socket.on("chat message", function(msg) {
+    $("#messages").append($("<li>").text(msg));
+    window.scrollTo(0, document.body.scrollHeight);
+  });
 
+  $('form[name="createRoomForm"]').submit(function() {
+    socket.emit("create room", $("#room").val(), $("#password").val());
+    $("#room").val("");
+    $("#password").val("");
+    return false;
+  });
 
+  socket.on("create room", function(room, password) {
+    $("#roomList").append(
+      "<li onclick = connectToRoom() > " +
+        "Room Name: " +
+        room +
+        " Password: " +
+        password
+      //   $("<li> <a href = '/lobby'> ").text(
+      //     "Room Name: " + room + " pass: " + password
+      //   )
+    );
+    //window.scrollTo(0, document.body.scrollHeight);
+  });
 });
+
+function connectToRoom() {
+  prompt("Password");
+}
