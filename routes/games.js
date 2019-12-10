a36e9e4a5b70ca35f53033db7a09b274729af4e2
Graves <theredhead242@gmail.com>
2019-12-10 02:06:29 -0800

    added game view

--- routes/games.js
+++ routes/games.js
@@ -8,10 +8,13 @@ router.get("/:id", isLoggedIn, function(request, response) {
   //id is just whatever it parses after /game_
   //params stores the id after game_
   //if we can get these we're golden
+
   const roomId = request.params["id"];
   const userId = request.user.id;
 
   console.log(userId + " joined " + roomId);
+
+  //check if the user is supposed to be in the lobby, or if it has two users already
 
   response.render("game");
 });
