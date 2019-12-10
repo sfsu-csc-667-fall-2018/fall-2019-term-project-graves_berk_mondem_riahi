2b192bd4405f10dc770c2f4fc40d7dd991455dbe
Yaboigraves <theredhead242@gmail.com>
2019-12-09 19:00:58 -0800

    room joining listener

--- public/javascripts/chatPost.js
+++ public/javascripts/chatPost.js
@@ -1,25 +1,31 @@
 //todo : remove all these repsonsibilities and defer them somewhere else
 
-$(function() {
+$(function () {
   var socket = io();
-  $('form[name="messageForm"]').submit(function() {
+  $('form[name="messageForm"]').submit(function () {
     socket.emit("chat message", $("#message").val());
     $("#message").val("");
     return false;
   });
-  socket.on("chat message", function(msg, time) {
+  socket.on("chat message", function (msg, time) {
     $("#messages").append($("<li>").text(time + " - " + msg));
     window.scrollTo(0, document.body.scrollHeight);
   });
 
-  $('form[name="createRoomForm"]').submit(function() {
+  $('form[name="createRoomForm"]').submit(function () {
     socket.emit("create room", $("#room").val(), $("#password").val());
     $("#room").val("");
     $("#password").val("");
     return false;
   });
 
-  socket.on("create room", function(room, password) {
-    $("#roomList").append("<li > " + "Room Name: " + room + "<button > Join");
+  socket.on("create room", function (room, password) {
+    let buttonTag = "<button onClick = 'joinRoom(" + '"' + room + '"' + ")' > Join";
+    $("#roomList").append("<li > " + "Room Name: " + room + buttonTag);
   });
 });
+
+
+function joinRoom(roomName) {
+  console.log("joining room " + roomName)
+}
