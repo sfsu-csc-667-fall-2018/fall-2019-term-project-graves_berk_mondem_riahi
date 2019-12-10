4995c80b6f1f03adbc15eb6db8443dabd4d2a31c
Graves <theredhead242@gmail.com>
2019-12-10 01:46:06 -0800

    roomid and userid revealed in games route

--- routes/games.js
+++ routes/games.js
@@ -3,15 +3,15 @@ const router = express.Router();
 const isLoggedIn = require("../auth/middleware/isLoggedIn");
 
 /* GET home page. */
-router.get("/:id", isLoggedIn, function (request, response) {
+
+router.get("/:id", isLoggedIn, function(request, response) {
   //id is just whatever it parses after /game_
   //params stores the id after game_
-
   //if we can get these we're golden
-  const { id: roomId } = request.params;
-  const { id: userId } = request.user;
+  const roomId = request.params["id"];
+  const userId = request.user.id;
 
-
+  console.log(userId + " joined " + roomId);
 
   response.render("game");
 });
