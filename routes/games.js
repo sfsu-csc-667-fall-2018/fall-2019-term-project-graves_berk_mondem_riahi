a5d24e175e3ebbcf7d539170f7aa731452c42172
Graves <theredhead242@gmail.com>
2019-12-12 01:21:42 -0800

    room joining logic prototype

--- routes/games.js
+++ routes/games.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const isLoggedIn = require("../auth/middleware/isLoggedIn");
 const db = require("./db/connection");
+// const deck = require("../gameLogic/deck");
 
 /* GET home page. */
 router.get("/:id", isLoggedIn, function(request, response) {
@@ -12,16 +13,50 @@ router.get("/:id", isLoggedIn, function(request, response) {
   const userId = request.user.id;
 
   //check if the user is supposed to be in the lobby, or if it has two users already
-  db.one("SELECT * FROM rooms WHERE room_id = $1", request.params["id"])
+  db.one("SELECT * FROM rooms WHERE room_id = $1", roomId)
     .then(_ => {
       console.log(userId + " joined " + roomId);
-      //initialize the deck and shit
-      response.render("game");
+      //add the user to the game
+      joinGame(userId, roomId, response);
     })
     .catch(error => {
-      response.render("lobby");
+      //if the game room doesnt exist just send them back to the lobby
+      response.redirect("lobby");
     });
 });
+
+function joinGame(userId, roomId, response) {
+  db.any("SELECT * FROM players WHERE room_id = $1", roomId)
+    .then(results => {
+      //if theirs one player in the room, then add them to the room, otherwise bounce them back to lobby
+      console.log(results);
+
+      //check the player(s) in the results and see if the given userid trying to join is either of them
+      let foundPlayer = false;
+      for (let i = 0; i < results.length; i++) {
+        if ([results[i]["user_id"]] == userId) {
+          foundPlayer = true;
+        }
+      }
+      //rendering
+      if (results.length == 0) {
+        console.log("makin the room");
+        db.none(
+          `INSERT INTO players (user_id,room_id) VALUES ('${userId}','${roomId}')`
+        ).then(response.render("game"));
+      } else if (foundPlayer) {
+        response.render("game");
+      } else {
+        response.redirect("/lobby");
+      }
+    })
+    .catch(error => {
+      //no players for that room
+      //get the user id who made the server and add them as a player for that room
+      //send them to the game
+      response.redirect("/lobby");
+    });
+}
 
 // drawing a card from the deck
 // //psuedo code
