77ebd0d5b0f00525386860a7c4c16f1d0f9f0bff
Graves <theredhead242@gmail.com>
2019-12-12 22:43:46 -0800

    guest and host recognition for room joining working

--- routes/games.js
+++ routes/games.js
@@ -22,7 +22,6 @@ router.get("/:id", isLoggedIn, function(request, response) {
     .catch(error => {
       //if the game room doesnt exist just send them back to the lobby
       console.log(error);
-      console.log("no rooms");
       response.redirect("/lobby");
     });
 });
@@ -63,79 +62,65 @@ function joinGame(userId, roomId, response) {
     else if (results.length == 1) {
       //if theyre not the host then add them as a guest
       //if they are the host then just let them back in and dont do anything
-      response.render("game");
-      console.log("should be a guest");
+
+      //get the player id of the user in the game currently
+
+      //check if the playerId is owned by the same user that is trying to join the lobby
+      //if it is owned by the same user, they are the host and just let them in
+      //if it is not the same user, then add the user as a new player to the game, then insert them as the guest_id in the rooms table
+
+      db.one(`SELECT * FROM players WHERE user_id = $1 AND room_id = $2`, [
+        userId,
+        roomId
+      ])
+        .then(results => {
+          let playerInRoomId = results["player_id"];
+
+          db.any(
+            `SELECT * FROM players WHERE player_id = $1 AND user_id = $2`,
+            [playerInRoomId, userId]
+          )
+            .then(results => {
+              //if this returns a result, that means that the player in the room is owned by this user, so just let them into the game
+              if (results.length > 0) {
+                //the user joining is the hosst
+                response.render("game");
+              } else {
+              }
+            })
+            .catch(error => {
+              console.log(error);
+            });
+        })
+        .catch(_ => {
+          //the user joining is a guest
+
+          //if theyre a guest and joining, then make a player for them, and add that players id to the guest_id slot for the room
+          db.none(
+            `INSERT INTO players (user_id,room_id) VALUES ('${userId}','${roomId}')`
+          )
+            .then(_ => {
+              //grab the player_id that was just generated
+              db.any(`SELECT * FROM players WHERE user_id = $1`, [userId]).then(
+                results => {
+                  //add that playerid as the hostid
+                  db.none(
+                    `UPDATE rooms SET guest_id = ${results[0]["player_id"]} WHERE room_id = $1`,
+                    [roomId]
+                  ).catch(error => {
+                    console.log(error);
+                  });
+                }
+              );
+            })
+            .then(response.render("game"));
+        });
     }
     //boot them back to the lobby
     else {
-      console.log("game full");
       response.redirect("/lobby");
     }
-
-    //rendering
-
-    //add the user to the players table
-
-    // db.none(
-    //   `INSERT INTO players (user_id,room_id) VALUES ('${userId}','${roomId}')`
-    // ).then(_ => {
-    //   //get the player id and then store that player id into the rooms table as a guest or host
-    //   db.any(`SELECT * from players WHERE user_id = $1 AND room_id = $2`, [
-    //     userId,
-    //     roomId
-    //   ])
-    //     .then(results => {
-    //       // console.log("i think this player is in this game");
-    //       // console.log(results);
-
-    //       db.none(
-    //         `UPDATE rooms SET ${guestOrHost} = ${results[guestOrHostPos]["player_id"]} WHERE room_id = $1`,
-    //         [roomId]
-    //       ).catch(error => console.log(error));
-    //     })
-    //     .catch(error => console.log(error));
-    // });
-
-    //   response.render("game");
-    // } else if (foundPlayer) {
-    //   response.render("game");
-    // } else {
-    //   response.redirect("/lobby");
-    // }
-
-    //rooms stuff
   });
-
-  // .catch(error => {
-  //   //no players for that room
-  //   //get the user id who made the server and add them as a player for that room
-  //   //send them to the game
-  //   response.redirect("/lobby");
-  // });
 }
-
-// drawing a card from the deck
-// //psuedo code
-// room.post("/:id/drawCard", function(request, response) {
-//   //gets you the socket info so you can emit stuff to the frontend
-//   let io = request.app.get("io");
-//   //orderNumber keeps track of the shuffled db
-//   //something like this
-//   let cardDrawn;
-//   db.one("SELECT * FROM deck MIN(deck.order_num) WHERE deck.room_id = " + roomId, [true]).then(card => {
-//     cardDrawn = card.card_id;
-//     //add that card_id to the hand table
-//     //remove the card_id from the deck table
-//   });
-
-//   //all the db bullshit is done
-//   //socket emmision
-//   io.to("namespace").emit(
-//     "draw card",
-//     request.body.roomName,
-//     request.body.roomId
-//     // request.body.roomPassword
-//   );
-// });
 
 module.exports = router;
