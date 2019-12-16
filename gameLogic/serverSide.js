2dab6616fddb2f9a236ae0bb52369329ba885c30
Matthew Berkman <kingprawn@grenadefish.net>
2019-12-16 03:52:36 -0800

    begun implementing score. and other things

--- gameLogic/serverSide.js
+++ gameLogic/serverSide.js
@@ -1,15 +1,7 @@
 const db = require("../routes/db/connection");
 const sortArray = require("array-sort"); //needed so can sort array in generateRandomDeck.
 
-/*
-var sets = []; //exist here so other player can get them after meld creation during scoring
-var runs = []; //exist here so other player can get them after meld creation during scoring
-var deadwoodList = []; //exist here so other player can get them after meld creation during scoring
-var smallestDeadwoodValue = 7331; //exist here so other player can get them after meld creation during scoring
-// ALSO exist for meld recursion
-var deadWoodCardCount = 1337; //exist here so other player can get them after meld creation during scoring
-// ALSO exist for meld recursion
- */
+
 
 async function getHand(playerId, roomId) {
   /*
@@ -58,6 +50,31 @@ async function getTopDiscard(roomId){
   return cardId;
 }
 
+async function checkDiscard(roomId){
+  let cardId
+
+  await db
+      .tx(async t => {
+        //seems this code will run async to rest of code in this method.
+        let results = await t.one(
+            "SELECT * FROM discards WHERE discard_id = (SELECT MAX(discard_id) FROM discards WHERE room_id = $1)",
+            roomId
+        );
+        cardId = results["card_id"];
+
+      })
+      .catch(error => {
+        console.log("Error in drawFromDiscard " + error);
+      });
+
+  if(cardId != undefined){
+    return true;
+  }
+
+  return false;
+}
+
+//todo PROBABLY when you return melds, you also return whether player can knock,gin,big big.
 async function getMeldData(playerId, roomId) {
   let playerHand = await getHand(playerId, roomId);
   ///console.log("IN getMeldData  " + playerHand);
@@ -276,7 +293,38 @@ async function deckToDiscard(roomId){
 
 
 
+//todo note, acitonPerformed will be value 1-3. 1 is knock, 2 is gin, 3 is big gin.
+//  will also send back score, not going to implemnt displaying new runs
+function doLayOffsAndScore(player1Id,player2Id,buttonPresserId,roomId,actionPerformed){
+    
+}
+
+
+
 //TODO above methods involves communication with client and database
+
+//todo PROBABLY when you return melds, you also return whether player can knock,gin,big big.
+
+//todo not going to make this function involve database things, will have a seperate function which calls this one.
+// was tired and made it super jank.  knocker, ginner, superGin will be boolean
+//   WILL need to calculate score before method call and also pass in what action was taken.
+//    also need to do layoffs before this method call.
+
+//for determining the score based on action and the deadwood value between the two players.
+function scoring(totalDeadwood, knocker, ginner, superGin, undercut){
+
+  if(knocker){
+    if(undercut){
+      return 25 + totalDeadwood;
+    }
+    return totalDeadwood
+  }else if(ginner){
+    return 25 + totalDeadwood;
+  }else if(superGin){
+    return  31 + totalDeadwood;
+  }
+}
+
 
 function sorted(listToSort) {
   /* //code that won't work in node.js
@@ -465,6 +513,8 @@ function formMelds(theHand) {
   }
 
   //todo didn't include players current hand since that should be done somewhere else I feel, like in game.js
+
+  //todo  ALSO CHECK if player can knock, gin, big gin and return values to client.
   let meldData = {
     runs: runs,
     sets: sets,
